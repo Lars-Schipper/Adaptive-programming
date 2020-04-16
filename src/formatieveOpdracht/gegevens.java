3bdaf2ca6a2d9a1e4d68913a458277c937bb16d0
lars-schipper <breukelen05@gmail.com>
2020-04-16 14:10:28 +0200

    begin gemaakt aan deel 2

---
+++ src/formatieveOpdracht/gegevens.java
@@ -0,0 +1,15 @@
+package formatieveOpdracht;
+
+public class gegevens {
+
+    public static void main(String[] args){
+
+        student student1 = new student("lars", 18, 3, "geen" );
+
+        System.out.println("NAAM: " + student1.naam);
+        System.out.println("LEEFTIJD: " + student1.leeftijd);
+        System.out.println("CIJFERS: "+ student1.cijferlijst);
+        System.out.println("HUISDIER " + student1.huisdier);
+
+    }
+}
---
+++ src/formatieveOpdracht/student.java
@@ -0,0 +1,16 @@
+package formatieveOpdracht;
+
+public class student {
+
+    String naam;
+    int leeftijd;
+    int cijferlijst;
+    String huisdier;
+
+    public student(String naam, int leeftijd, int cijferlijst, String huisdier){
+        this.naam = naam;
+        this.leeftijd = leeftijd;
+        this.cijferlijst = cijferlijst;
+        this.huisdier = huisdier;
+    }
+}
